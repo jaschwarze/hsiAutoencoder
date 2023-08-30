7a0ce17eedfbbb0ff58d6303631078592c91e2d1
Jan-Philipp Schwarze <J.P.Schwarze@web.de>
2023-08-30 13:06:09 +0200

    added autoencoder model

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -1,116 +1,51 @@
+import keras
+from keras.models import Model
+from keras.layers import Input, Dense
 import tensorflow as tf
-import numpy as np
-
-mnist = tf.keras.datasets.mnist
-
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train = x_train / np.max(x_train)
-
-x_train = x_train.reshape(x_train.shape[0], x_train.shape[1] * x_train.shape[2])
-x_train = x_train.astype("float32")
-
-training_dataset = tf.data.Dataset.from_tensor_slices(x_train)
-training_dataset = training_dataset.batch(batch_size=32)
-training_dataset = training_dataset.shuffle(x_train.shape[0])
+from config import *
 
 
-class Encoder(tf.keras.layers.Layer):
-    def __init__(self, num_neurons):
-        super(Encoder, self).__init__()
-
-        self.hidden_layer1 = tf.keras.layers.Dense(
-            units=num_neurons,
-            activation=tf.nn.relu)
-
-        self.hidden_layer2 = tf.keras.layers.Dense(
-            units=num_neurons,
-            activation=tf.nn.relu)
-
-    def call(self, input_features):
-        """Call function for the decoder class
-
-        @param input_features: input features
-        @return output: reconstructed input features
-        """
-        activation = self.hidden_layer1(input_features)
-        return self.hidden_layer2(activation)
+def frobenius_loss(y_true, y_pred):
+    m = tf.cast(tf.shape(y_true)[0], dtype=tf.float32)
+    frobenius_norm = tf.norm(y_true - y_pred, ord="fro", axis=[-2, -1])
+    return frobenius_norm / (2 * m)
 
 
-class Decoder(tf.keras.layers.Layer):
-    def __init__(self, num_neurons, mnist_dim):
-        super(Decoder, self).__init__()
-
-        self.hidden_layer = tf.keras.layers.Dense(
-            units=num_neurons,
-            activation=tf.nn.relu)
-
-        self.output_layer = tf.keras.layers.Dense(
-            units=mnist_dim,
-            activation=tf.nn.relu)
-
-    def call(self, input_data):
-        """Call function for the decoder class
-
-        @param input_data: input features
-        @return output: reconstructed input features
-        """
-        activation = self.hidden_layer(input_data)
-        return self.output_layer(activation)
+def row_sparse_regularization(matrix):
+    return tf.reduce_sum(tf.norm(matrix, ord=2, axis=1))
 
 
-class Autoencoder(tf.keras.Model):
-    def __init__(self, num_neurons, mnist_dim):
+class Autoencoder(Model):
+    def __init__(self, input_dim, encoding_dim):
         super(Autoencoder, self).__init__()
+        self.input_dim = input_dim
+        self.encoding_dim = encoding_dim
 
-        self.encoder = Encoder(num_neurons)
+        input_layer = Input(shape=(self.input_dim,), name="input_layer")
+        hidden_layer = Dense(self.encoding_dim, activation="sigmoid", name="hidden_layer")(input_layer)
+        output_layer = Dense(self.input_dim, activation="linear", name="output_layer")(hidden_layer)
 
-        self.decoder = Decoder(num_neurons, mnist_dim)
+        self.autoencoder = Model(input_layer, output_layer)
+        self.encoder = Model(input_layer, hidden_layer)
 
-        # Define an optimizer
-        self.opt = tf.optimizers.Adam(learning_rate=0.01)
+        encoded_input = keras.Input(shape=(encoding_dim,), name="encoded_input")
+        decoder_layer = self.autoencoder.layers[-1]
+        self.decoder = keras.Model(encoded_input, decoder_layer(encoded_input))
+        self.autoencoder.compile(optimizer="adam", loss=self.combined_loss)
 
-    def call(self, x_train):
-        """Call function for the autoencoder class
+    def combined_loss(self, y_true, y_pred):
+        return (
+                frobenius_loss(y_true, y_pred)
+                + ALPHA * row_sparse_regularization(self.autoencoder.layers[1].weights[0])
+                + (BETA / 2) * (tf.norm(self.autoencoder.layers[1].weights[0], ord="fro", axis=[-2, -1]) + tf.norm(self.autoencoder.layers[2].weights[0], ord="fro", axis=[-2, -1]))
+        )
 
-        @param x_train: input features
-        @return output: reconstructed input features
-        """
+    def train(self, data, epochs, batch_size, shuffle, validation_data=None):
+        return self.autoencoder.fit(x=data, y=data, epochs=epochs, batch_size=batch_size, shuffle=shuffle,
+                                    validation_data=validation_data)
 
-        latent_representation = self.encoder(x_train)
-        output = self.decoder(latent_representation)
+    def encode(self, image):
+        return self.encoder.predict(image)
 
-        return output
-
-    def compute_loss(self, x_train):
-        """Compute MSE loss function.
-
-        @param x_train: input features
-        """
-
-        mse = tf.keras.losses.MeanSquaredError()
-        loss = mse(x_train, self(x_train))
-        return loss
-
-    def train(self, x_train):
-        """Train the autoencoder.
-
-        @parameter x_train: training input features
-        """
-
-        # Compute the gradients and apply the gradient descent step
-        with tf.GradientTape() as tape:
-            gradients = tape.gradient(self.compute_loss(x_train), self.trainable_variables)
-            gradient_variables = zip(gradients, self.trainable_variables)
-            self.opt.apply_gradients(gradient_variables)
-
-
-autoencoder = Autoencoder(num_neurons=64, mnist_dim=784)
-num_epoch = 5
-
-for epoch in range(num_epoch):
-
-    # Iterate over the batches
-    for step, x_train in enumerate(training_dataset):
-        autoencoder.train(x_train)
-        loss_values = autoencoder.compute_loss(x_train)
-        print(loss_values)
+    def decode(self, encoded_image):
+        return self.decoder.predict(encoded_image)
